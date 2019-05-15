c34a7338233a5069476fec8eb044549a21ac805f
channu <channucs0009@gmail.com>
2019-05-15 23:23:07 +0530

    conditional output

--- React-course/react-net-ninja/src/Ninjas.js
+++ React-course/react-net-ninja/src/Ninjas.js
@@ -1,14 +1,26 @@
 import React from 'react'
 const  Ninjas =({ninjas})=> {
         
-       const ninjalist= ninjas.map(ninja=>{
-            return(
-                <div key={ninja.id}>
-                     <h1>{ninja.name}</h1> <h1>{ninja.age}</h1>
-                </div>
-            )
-        })
-        return (ninjalist)
+    //    const ninjalist= ninjas.map(ninja=>{ 
+    //        if (ninja.age >22){
+    //         return(
+    //             <div key={ninja.id}>
+    //                  <h1>{ninja.name}</h1> <h1>{ninja.age}</h1>
+    //             </div>
+    //         )}
+    //         else
+    //         return null
+    //     })
+   
   
+  return (<div>{
+                ninjas.map(ninja =>{
+                    return ninja.age>20?( <div key={ninja.id}>
+                                         <h1>{ninja.name}</h1> <h1>{ninja.age}</h1>
+                                        </div>):null
+                                }
+                    )}
+            </div>
+        )
 }
 export default Ninjas;
