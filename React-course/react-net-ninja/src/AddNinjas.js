c03ba9bdab32f9555e41eebe12ff57dad78d95b2
channu <channucs0009@gmail.com>
2019-05-16 10:30:00 +0530

    Form handle

---
+++ React-course/react-net-ninja/src/AddNinjas.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class AddNinjas extends Component {
+    state={
+        name:null,
+        age:null,
+        mob:null
+        
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state)
+    }
+    handleChange=(e)=>{
+        this.setState({[e.target.id]:e.target.value})
+    }
+  render() {
+    return (
+            <div>
+            <form className="form-group" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" id="name" onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+            <label htmlFor="name">Age</label>
+            <input type="text" className="form-control" id="age"   onChange={this.handleChange}/>
+            </div> 
+            <div className="form-group">
+            <label htmlFor="name">Mobile</label>
+            <input type="text" className="form-control" id="mob"  onChange={this.handleChange} />
+            </div>
+            <button className="btn btn-primary btn-group-lg btn-block my-1">Submit</button>
+
+            </form>
+        </div>
+    )
+  }
+}
--- React-course/react-net-ninja/src/App.js
+++ React-course/react-net-ninja/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Ninjas from './Ninjas'
+import AddNinjas  from './AddNinjas'
 export default class App extends Component {
   state={
     ninjas:[{name:'channu',age:26,mob:'9980972009',id:1},
@@ -9,9 +10,12 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
+     
+      <div className="container bg-dark my-4 text-capitalize text-light">
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinjas />
       </div>
+     
     )
   }
 }
