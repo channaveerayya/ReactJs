df33c7205bf2a17f571e3304ed4945c625606f9e
channu <channucs0009@gmail.com>
2019-05-16 21:02:38 +0530

    Todos part-1 Delete-todos

---
+++ net-ninja-todo-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Todos from './Todos'
+export default class App extends Component {
+  state={
+    todos:[
+      {id:1,content:'bye some milk'},
+      {id:2,content:'play mario kart'}
+    ]
+  }
+  deleteTodo=(id)=>{
+    const todos=this.state.todos.filter(todo=>{
+      return todo.id!==id
+    });
+    
+    this.setState({todos})
+  }
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    )
+  }
+}
