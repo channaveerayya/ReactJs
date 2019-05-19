a917e0510abda95cc9e34e3de4162222c59d64b2
channu <channucs0009@gmail.com>
2019-05-19 15:29:19 +0530

    switch

--- React-course/react-net-ninja-part-2/src/App.js
+++ React-course/react-net-ninja-part-2/src/App.js
@@ -1,21 +1,24 @@
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact';
+import Posts from './components/Posts';
+
 export default class App extends Component {
  render() {
     return (
      <BrowserRouter>
       <div className="App">
         <Navbar  />
+        <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
-
-
+        <Route path='/:post_id' component={Posts} />
+        </Switch>
       </div>
       </BrowserRouter>
     )
--- React-course/react-net-ninja-part-2/src/components/Navbar.js
+++ React-course/react-net-ninja-part-2/src/components/Navbar.js
@@ -14,6 +14,7 @@ const Navbar=(props)=> {
                   <li><Link to='/'>Home</Link></li>
                   <li><NavLink to='/about'>About</NavLink></li>
                   <li><NavLink to='/contact'>Contact</NavLink></li>
+                  
                   </ul>    
           </div>
       </nav>
