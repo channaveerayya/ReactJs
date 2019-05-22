afd5169ef49fbbab0a4a69d96bb6a62d09be16bf
channu <channucs0009@gmail.com>
2019-05-22 15:17:42 +0530

    Event Handle-- Toggle prg

--- react-official-doc/src/App.js
+++ react-official-doc/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Clock from './examples/Clock';
 import Counter from './examples/Counter'
+import Toggle from './examples/Toggle'
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
       <Clock />
+      <Toggle />
       
     </div>
   );
