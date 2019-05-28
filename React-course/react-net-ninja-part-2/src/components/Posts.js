26112122a963680ada051f3f61708d72d4778e77
channu <channucs0009@gmail.com>
2019-05-28 14:13:32 +0530

    map dispatc to props and deleting

--- React-course/react-net-ninja-part-2/src/components/Posts.js
+++ React-course/react-net-ninja-part-2/src/components/Posts.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import { connect  } from "react-redux";
 import { Home } from './Home';
  class Posts extends Component {
+   handleClick=()=>{
+     this.props.deletePost(this.props.post.id);
+     this.props.history.push('/');
+   }
    
   render() {
-      console.log(this.props.match.params.post_id)
+      console.log("hello",this.props)
       const post=this.props.post ? (
           <div className="post">
               <h4 className="center">{this.props.post.title}</h4>
               <p>{this.props.post.body}</p>
+              <div className="center">
+                  <button className="btn grey" onClick={this.handleClick}>
+                    Click Here
+                  </button>
+              </div>
           </div>
       ) :(
       <div className="center">Loading post....</div>
@@ -22,9 +31,16 @@ import { Home } from './Home';
 }
 const mapStateToProps=(state,ownProps)=>{
   let id=ownProps.match.params.post_id
-  console.log(state);
+  console.log("state",state);
   return({
     post:state.posts.find(post=>post.id===id)
   })
 }
-export default connect(mapStateToProps)  (Posts)
+const mapDispatchProps=(dispatch)=>{
+  return{
+    deletePost:(id)=>{
+      dispatch({type:'DELETE_POST',id:id})
+    }
+  }
+}
+export default connect(mapStateToProps,mapDispatchProps)  (Posts)
