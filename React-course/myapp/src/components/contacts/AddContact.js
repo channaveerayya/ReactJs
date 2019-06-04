a968856e862cea49230b005a5e276b514496e916
channu <channucs0009@gmail.com>
2019-06-04 12:12:57 +0530

    POST Requests

--- React-course/myapp/src/components/contacts/AddContact.js
+++ React-course/myapp/src/components/contacts/AddContact.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Consumer} from '../../context'
 import uuid from 'uuid'
 import TextInputGroup from '../layout/TextInputGroup'
+import axios from 'axios'
 class AddContact extends Component {
 
 
@@ -30,13 +31,15 @@ class AddContact extends Component {
         }
         
         const newContact={
-            id:uuid(),
             name,
             email,
             phone
             
         };
-        dispatch({type:'ADD_CONTACT',payload:newContact});
+        axios.post('https://jsonplaceholder.typicode.com/users',newContact)
+            .then(res=>dispatch({type:'ADD_CONTACT',payload:res.data}));
+
+        
     
 
     //clear state
